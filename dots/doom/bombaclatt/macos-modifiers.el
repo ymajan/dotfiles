0940c896d4e6be91ad62932ac4b5c7469bb1d27f
Jaynams Ahh <ymajan@jaynam.me>
2025-04-10 14:48:10 -0400

    cleaned up

--- dots/doom/bombaclatt/macos-modifiers.el
+++ dots/doom/bombaclatt/macos-modifiers.el
@@ -1,5 +1,5 @@
 ;; Setting up modifier keys for macOS
 (setq mac-command-modifier  'super        ; Command (⌘) key     <-> Super (S-)
       mac-option-modifier   'meta         ; Option/Alt (⌥) key  <-> Meta (M-)
-      mac-function-modifier 'hyper        ; Fn (🌐) key         <-> Hyper (H-)
-      ns-control-modifier   'control)     ; Control (⌃) key     <-> Control (C-)
+      mac-function-modifier 'hyper)       ; Fn (🌐) key         <-> Hyper (H-)
+;;    ns-control-modifier   'control      ; Control (⌃) key     <-> Control (C-)
