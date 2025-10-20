9c41c7fbe2458722d07106cc45003a3f2aede668
ymajan <github@jaynam.me>
2025-10-20 17:03:15 -0400

    we switching up once more

--- unmanaged/guix/system/config.scm
+++ unmanaged/guix/system/config.scm
@@ -1,14 +1,3 @@
-;; This is an operating system configuration generated
-;; by the graphical installer.
-;;
-;; Once installation is complete, you can learn and modify
-;; this file to tweak the system configuration, and pass it
-;; to the 'guix system reconfigure' command to effect your
-;; changes.
-
-
-;; Indicate which modules to import to access the variables
-;; used in this configuration.
 (use-modules 
   (gnu) 
   (gnu packages shells)
---
+++ unmanaged/guix/system/symlinks.md
@@ -0,0 +1,9 @@
+cd /etc and symlink config.scm with
+```
+sudo ln -s /home/ymajan/dotfiles/geeks/config.scm
+```
+
+cd /etc/guix and symlink channels.scm with
+```
+sudo ln -s /home/ymajan/dotfiles/geeks/channels.scm
+```
