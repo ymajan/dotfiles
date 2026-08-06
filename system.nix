76ae7e3edb1cfaa00cc22edad9cc987f7d60d612
Jaynam Shah <github@ymajan.pw>
2026-08-06 17:09:17 -0400

    pin SSH hosts and export Bitwarden agent

--- system.nix
+++ system.nix
@@ -1,4 +1,10 @@
-{ self, ... }: {
+{ self, ... }:
+
+let
+  bitwardenSshAgent =
+    "/Users/ymajan/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock";
+in
+{
   system.primaryUser = "ymajan";
 
   networking = {
@@ -12,6 +18,24 @@
   users.users.ymajan = {
     name = "ymajan";
     home = "/Users/ymajan";
+  };
+
+  # Make the Bitwarden agent available to GUI applications launched by macOS.
+  launchd.user.envVariables.SSH_AUTH_SOCK = bitwardenSshAgent;
+
+  # Pin server identities independently from the user authentication keys.
+  programs.ssh.knownHosts = {
+    komodo = {
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/nMx8IIknCT5TaZlP1WHT+jcbpX5aW/mfY4VoVzaTa";
+    };
+
+    "jsi-root" = {
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB0YyBKsh0UkH+6UHUHM+Ws1GfBZaGmHbCp8D2kHQWXC";
+    };
+
+    "jsi-agent" = {
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/2LG9qR0EtnSVsYRPW1cBx2OLp82EynhiR1PPti1gH";
+    };
   };
 
   system.configurationRevision = self.rev or self.dirtyRev or null;
